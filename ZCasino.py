c24adffd8ce9eadf018058d2a971e1b79c6c2a87
AnthonyP06 <a.pagliai06@gmail.com>
2015-09-11 17:18:07 +0200

    Deletion of an useless variable (2)

--- ZCasino.py
+++ ZCasino.py
@@ -2,9 +2,6 @@
 from random import randrange
 from math import ceil
 
-# Check the game over ?
-isOver = False
-
 # How much does the player want to bet ?
 money = input("How much do you want to play with ? ")
 print " " 
